6b09f3a06cfa804d3c838b22b0b3a821c94d85d6
Andreas Köberle <koeberle@gmail.com>
2012-06-02 16:38:57 +0200

    adding some 2d vector functions

--- src/math/vec2.dart
+++ src/math/vec2.dart
@@ -1,11 +1,43 @@
 class Vec2 extends Coordinate {
   const Vec2([num x = 0, num y = 0]) : super(x,y);
 
+  static Vec2 difference(Coordinate a, Coordinate b){
+    return new Vec2(a.x - b.x, a.y - b.y);
+  }
+  
   Vec2 operator +(Coordinate other){
     return new Vec2(x + other.x, y + other.y);
   }
   
-  static Vec2 difference(Coordinate a, Coordinate b){
-    return new Vec2(a.x - b.x, a.y - b.y);
+  Vec2 operator -(Coordinate other) { 
+      return new Vec2(x - other.x, y - other.y);
+  } 
+  
+  Vec2 operator *(Coordinate other) { 
+      return new Vec2(x * other.x, y * other.y);
+  } 
+  
+  Vec2 operator /(Coordinate other) { 
+      return new Vec2(x / other.x, y / other.y);
+  } 
+  
+  Vec2 operator %(Coordinate other) { 
+      return new Vec2(x % other.x, y % other.y);
+  } 
+  
+  num getDistance (Coordinate other) {
+    num distX = other.x - x;
+    num distY = other.y - y;
+    return Math.sqrt(distX * distX + distY * distY);
   }
+  
+  num dot(Vec2 other) =>  x * other.x + y * other.y;
+  
+  num cross(Vec2 other) => x * other.y - y * other.x;
+  
+  num getAngle (Vec2 other) => Math.acos(dot(other));
+  
+
+  String toString() => '{"x":${x},"y":${y}}';
+  
 }
---
+++ test/math/test_vec2.dart
@@ -0,0 +1,46 @@
+class TestVec2 {
+  static void run(){
+    group('Vec2 -- ', (){
+      test('should be sum with other Vec2', (){
+        Vec2 v = new Vec2(1,1) + new Vec2(2,1);
+        Expect.equals(3, v.x);
+        Expect.equals(2, v.y);
+      });
+      
+      test('should be subtract by other Vec2', (){
+        Vec2 v = new Vec2(5,3) - new Vec2(2,1);
+        Expect.equals(3, v.x);
+        Expect.equals(2, v.y);
+      });
+      
+      test('should be multiply by other Vec2', (){
+        Vec2 v = new Vec2(2,3) * new Vec2(2,3);
+        Expect.equals(4, v.x);
+        Expect.equals(9, v.y);
+      });
+      
+      test('should be divided by other Vec2', (){
+        Vec2 v = new Vec2(4,8) / new Vec2(2,4);
+        Expect.equals(2, v.x);
+        Expect.equals(2, v.y);
+      });
+      
+      test('should be compared by other Vec2', (){
+        Expect.isTrue(new Vec2(2,2) == new Vec2(2,2));
+        Expect.isTrue(new Vec2(2,1) != new Vec2(2,2));
+      });
+      
+      test('should get the distance to another point', (){
+        Expect.equals(5, new Vec2(0, 0).getDistance(new Vec2(3, 4)));
+      });
+      
+      test('should calc the dot product', (){
+        Expect.equals(23, new Vec2(2, 3).dot(new Vec2(4, 5)));
+      });
+      
+      test('should calc the cross product', (){
+        Expect.equals(-2, new Vec2(2, 3).cross(new Vec2(4, 5)));
+      });
+    });
+  }
+}
