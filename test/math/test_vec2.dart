d89e15bc81bcc195b18085e67f35c818f70586c7
Andreas Köberle <koeberle@gmail.com>
2012-06-03 00:37:34 +0200

    Docs and new methods for Vec2
    * Adding docs
    * adding length and copy method to Vec2

--- test/math/test_vec2.dart
+++ test/math/test_vec2.dart
@@ -34,12 +34,23 @@ class TestVec2 {
         Expect.equals(5, new Vec2(0, 0).getDistance(new Vec2(3, 4)));
       });
       
+      test('should get length of the vector', (){
+        Expect.equals(5, new Vec2(3, 4).length());
+      });
+      
       test('should calc the dot product', (){
         Expect.equals(23, new Vec2(2, 3).dot(new Vec2(4, 5)));
       });
       
       test('should calc the cross product', (){
         Expect.equals(-2, new Vec2(2, 3).cross(new Vec2(4, 5)));
+      });
+      
+      test('should create a copy of itself', (){
+        Vec2 original = new Vec2(1,2);
+        Vec2 copy = original.copy();
+        Expect.equals(copy.x, original.x);
+        Expect.equals(copy.y, original.y);
       });
     });
   }
