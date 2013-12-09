c888e1c2869e8d5832ba5bf2adde234c9ac2bdd7
Kevin Moore <kevin@thinkpixellab.com>
2013-12-09 15:44:06 -0600

    graph: final move-around

--- lib/src/bot/graph.dart
+++ lib/src/bot/graph.dart
@@ -5,87 +5,61 @@ import 'dart:collection';
 import 'require.dart';
 
 part 'graph/topo_sort.dart';
+part 'graph/tarjan.dart';
 
-/**
- * [Tarjan's strongly connected components algorithm](http://en.wikipedia.org/wiki/Tarjan's_strongly_connected_components_algorithm)
- */
-List<List> stronglyConnectedComponents(Map<dynamic, Iterable> graph) {
-  requireArgumentNotNull(graph, 'graph');
+class _Graph<T> {
+  final LinkedHashMap<T, _GraphNode<T>> _map;
 
-  var nodes = new _Graph(graph);
-  var tarjan = new _TarjanCycleDetect(nodes);
-  return tarjan.calculate();
+  factory _Graph(Map<T, Iterable<T>> items) {
+
+    var map = new LinkedHashMap<T, _GraphNode<T>>();
+
+    _GraphNode<T> getNode(T value) =>
+        map.putIfAbsent(value, () => new _GraphNode<T>(value));
+
+    items.forEach((T item, Iterable<T> outLinks) {
+      if(outLinks == null) outLinks = [];
+
+      var node = getNode(item);
+      for(T outLink in outLinks) {
+        var newItem = node.outNodes.add(getNode(outLink));
+        requireArgument(newItem, 'items', 'Outlinks must not contain dupes');
+      }
+
+    });
+
+    return new _Graph.core(map);
+  }
+
+  _Graph.core(this._map);
+
+  Iterable<_GraphNode<T>> get nodes => _map.values;
+
+  String toString() {
+    var sb = new StringBuffer();
+    sb.writeln('{');
+    _map.values.forEach((_GraphNode<T> value) {
+      var outNodeStr = value.outNodes
+          .map((gn) => gn.value)
+          .join(', ');
+
+      sb.writeln('  ${value.value} => {$outNodeStr}');
+    });
+
+    sb.writeln('}');
+    return sb.toString();
+  }
+
 }
 
-/**
- * Use top-level [stronglyConnectedComponents] instead.
- */
-@deprecated
-class TarjanCycleDetect<TNode> {
+class _GraphNode<T> {
+  final T value;
+  final LinkedHashSet<_GraphNode> outNodes = new LinkedHashSet<_GraphNode<T>>();
 
-  /**
-   * Use top-level [stronglyConnectedComponents] instead.
-   */
-  @deprecated
-  static List<List> getStronglyConnectedComponents(Map graph) =>
-      stronglyConnectedComponents(graph);
-}
+  _GraphNode(this.value);
 
-class _TarjanCycleDetect<T> {
-  final _indexExpando = new Expando<int>('index');
-  final _linkExpando = new Expando<int>('link');
+  bool operator ==(Object other) =>
+      other is _GraphNode<T> && other.value == value;
 
-  final Queue<_GraphNode> _stack = new Queue<_GraphNode<T>>();
-  final List<List<T>> _scc = new List<List<T>>();
-  final _Graph<T> _list;
-
-  int _index = 0;
-
-  _TarjanCycleDetect(this._list);
-
-  List<List<T>> calculate() {
-    for (var node in _list.nodes) {
-      if(_getIndex(node) == -1) {
-        _tarjan(node);
-      }
-    }
-    return _scc;
-  }
-
-  void _tarjan(_GraphNode<T> v) {
-    _setIndex(v, _index);
-    _setLowLink(v, _index);
-    _index++;
-    _stack.addFirst(v);
-    for(final n in v.outNodes){
-      if(_getIndex(n) == -1){
-        _tarjan(n);
-        _setLowLink(v, math.min(_getLowLink(v), _getLowLink(n)));
-      } else if(_stack.contains(n)){
-        _setLowLink(v, math.min(_getLowLink(v), _getIndex(n)));
-      }
-    }
-    if(_getLowLink(v) == _getIndex(v)){
-      _GraphNode n;
-      var component = new List<T>();
-      do {
-        n = _stack.removeFirst();
-        component.add(n.value);
-      } while(n != v);
-      _scc.add(component);
-    }
-  }
-
-  int _getIndex(_GraphNode<T> node) {
-    var value = _indexExpando[node];
-    return (value == null) ? -1 : value;
-  }
-
-  int _setIndex(_GraphNode<T> node, int value) =>
-    _indexExpando[node] = value;
-
-  int _getLowLink(_GraphNode<T> node) => _linkExpando[node];
-
-  int _setLowLink(_GraphNode<T> node, int value) =>
-    _linkExpando[node] = value;
+  int get hashCode => value.hashCode;
 }
